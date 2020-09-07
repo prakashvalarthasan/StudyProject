627e89daba4513d338fcc487b3fa74df28fbb500
prakashvalarthasan <grittierprakash1@gmail.com>
2020-09-07 17:27:12 +0530

    code moved to grittier

--- src/test/java/org/hexa/DemoPractice.java
+++ src/test/java/org/hexa/DemoPractice.java
@@ -3,5 +3,8 @@ package org.hexa;
 public class DemoPractice {
 public static void main(String[] args) {
 	System.out.println("This is a testing program");
+	String s = "java";
+	String sst = s.substring(2);
+	System.out.println(sst);
 }
 }
