1ec7812529a3d3f5cf1341f6b4c98cf948288ca6
prakashvalarthasan <grittierprakash1@gmail.com>
2020-09-07 17:36:39 +0530

    code for gritty

--- src/test/java/org/hexa/DemoPractice.java
+++ src/test/java/org/hexa/DemoPractice.java
@@ -3,5 +3,8 @@ package org.hexa;
 public class DemoPractice {
 public static void main(String[] args) {
 	System.out.println("This is a testing program");
+	String a = "do not give up";
+	String trim = a.trim();
+	System.out.println(trim);
 }
 }
